73a1233b2996d488e70bbabd51ea5e02474f85e4
James Nguyen <nguyenjtq@gmail.com>
2022-09-06 22:20:50 -0700

    added jamf popup window to script

--- setComputerName.sh
+++ setComputerName.sh
@@ -16,10 +16,13 @@ ENDofOSAscript
 /usr/sbin/scutil --set ComputerName $computerName
 /usr/sbin/scutil --set LocalHostName $computerName
 
+##############################
+# Jamf Helper Popup Window 1 #
+##############################
+/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -other_options foo -startlaunchd -windowType hud -description "Changed computer name to: $computerName" -icon /Applications/Self\ Service.app/Contents/Resources/Self\ Service.icns &
+
 ####################################
 # First Recon to set Computer Name #
 ####################################
 /bin/echo "Running recon"
 /usr/local/jamf/bin/jamf recon
-
-/bin/echo "Changed computer names to: $computerName"
