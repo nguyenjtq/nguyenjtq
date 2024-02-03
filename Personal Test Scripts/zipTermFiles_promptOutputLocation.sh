c7569864503ec08c59eec0472622c01eb332a707
James Nguyen <nguyenjtq@gmail.com>
2024-02-02 16:39:38 -0800

    Updated logic to allow voluntary continuing despite disk utilization

--- Personal Test Scripts/zipTermFiles_promptOutputLocation.sh
+++ Personal Test Scripts/zipTermFiles_promptOutputLocation.sh
@@ -40,7 +40,9 @@ intDiskCap="${diskCap//%}"
 #####################################
 askInput() {
     userFolder=$(sudo -u "$loggedInUser" /usr/bin/osascript <<EOF
+        --initiate variable
         set theTextReturned to "nil"
+        -- have user choose which user folder to back up
         try
             tell app "System Events"
                 activate
@@ -55,13 +57,18 @@ askInput() {
         end if
 EOF
     )
+    ## Exit script if userFolder prompt is cancelled
     if [ "$userFolder" == "cancelled" ]; then
         /usr/bin/osascript -e "display dialog \"Zip cancelled\" with title \"$orgName Self Service\" buttons {\"Close\"} default button 1 with icon POSIX file \"$brandIcon\""
         exit 0
     fi
 
+    ## Prompt to choose destination location.
+    ## Use either ~/Desktop or External Volume. If External Volume, list /Volumes folder and choose.
     outputFolder=$(sudo -u "$loggedInUser" /usr/bin/osascript <<EOF
+        -- initiate variable
         set chosenVolume to "nil"
+        -- have user choose between local desktop or external volume
         try
             tell app "System Events"
                 activate
@@ -73,7 +80,9 @@ EOF
             return "cancelled"
         else if chosenVolume is "~/Desktop" then
             return chosenVolume
+        -- essentially if chosenVolume == "External Volume", list and choose among /Volumes
         else
+            set chosenVolume to "nil"
             try
                 tell app "System Events"
                     activate
@@ -81,20 +90,31 @@ EOF
                     set chosenVolume to the item 1 of theResponse
                 end tell
             end try
-            return chosenVolume
+            if chosenVolume is "nil" then
+                return "cancelled"
+            else
+                return chosenVolume
+            end if
         end if
 EOF
     )
+    ## Exit script if outputFolder prompt is cancelled
     if [ "$outputFolder" == "cancelled" ]; then
         /usr/bin/osascript -e "display dialog \"Zip cancelled\" with title \"$orgName Self Service\" buttons {\"Close\"} default button 1 with icon POSIX file \"$brandIcon\""
         exit 0
     fi
-    
     case "$outputFolder" in
+        ## If output folder is "Desktop", check local disk utilization. Cancel or continue script accordingly.
         "~/Desktop" )
-            if [ "$intDiskCap" -le "50" ]; then
-                /usr/bin/osascript -e "display dialog \"Disk usage is $diskCap, you will need a formatted (exFAT, FAT32, APFS) external drive with sufficient storage. Plug one in and restart the process.\" with title \"$orgName Self Service\" buttons {\"Close\"} default button 1 with icon POSIX file \"$brandIcon\""
+            ## If disk utilization >= 50%, prompt for yes/no to continue or cancel.
+            if [ "$intDiskCap" -ge "50" ]; then
+                button=$(/usr/bin/osascript -e "display dialog \"Disk usage is $diskCap, you may need a formatted (exFAT, FAT32, APFS) external drive with sufficient storage. Would you like to continue?.\" with title \"$orgName Self Service\" buttons {\"No\", \"Yes\"} default button 1 with icon POSIX file \"$brandIcon\"")
+            fi
+            ## If no, cancel and exit script.
+            if [ "$button" == "button returned:No" ]; then
+                /usr/bin/osascript -e "display dialog \"Zip cancelled\" with title \"$orgName Self Service\" buttons {\"Close\"} default button 1 with icon POSIX file \"$brandIcon\""
                 exit 0
+            ## If yes, continue with script, though depending on utilization, this may result in `zip I/O error: no space left on device` error by `zip` command.
             else
                 ## Output file location
                 zipSaveLocation="/Users/$loggedInUser/Desktop/$(date '+%Y-%m-%d-%H.%M.%S')_$userFolder-$serialNumber.zip"
@@ -106,7 +126,9 @@ EOF
                 /usr/bin/osascript -e "tell application \"Terminal\" to activate" -e "tell application \"Terminal\" to do script \"sudo zip -ryv '$zipSaveLocation' '/Users/$userFolder' -x '/Users/$userFolder/Library/*' && open -R '$zipSaveLocation'\""
             fi
             ;;
+        ## Use "/Volumes" path as destination.
         * )
+            ## Output file location
             zipSaveLocation="/Volumes/$outputFolder/$(date '+%Y-%m-%d-%H.%M.%S')_$userFolder-$serialNumber.zip"
             
             ## Display zipping dialog
